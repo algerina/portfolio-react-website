657c386d42158f0c30567f254ffd2b69eb44a54a
Amel Khiri <53347966+algerina@users.noreply.github.com>
2022-09-05 21:52:09 +0100

    Add image and social media icons

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
+import Me from '../../assets/me.png'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
@@ -10,6 +12,12 @@ const Header = () => {
         <h1>Amel Khiri</h1>
         <h5 className="text-light">Fullstack Developer</h5>
         <CTA />
+        <HeaderSocials />
+
+        <div className="me">
+          <img src={Me} alt="me" />
+          <a href="#contact" className="scroll__down">Scroll Down</a>
+        </div>
       </div>
       
     </header>

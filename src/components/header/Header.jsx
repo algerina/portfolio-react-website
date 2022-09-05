48afe133c55e685388e8b6b70ebc24a35898690a
Amel Khiri <53347966+algerina@users.noreply.github.com>
2022-09-05 21:09:56 +0100

    Create header section and style buttons

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Amel Khiri</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+        <CTA />
+      </div>
+      
+    </header>
   )
 }
 

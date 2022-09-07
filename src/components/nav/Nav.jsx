840e590b844e390d339b3fce436e91f0161e145e
Amel Khiri <53347966+algerina@users.noreply.github.com>
2022-09-08 00:24:08 +0100

    Style navbar

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import './nav.css'
+import { AiOutlineHome } from 'react-icons/ai'
+import { AiOutlineUser } from 'react-icons/ai'
+import { BiBook } from 'react-icons/bi'
+import { CgWorkAlt } from 'react-icons/cg'
+import { BiMessageRoundedDetail } from 'react-icons/bi'
+
+
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#"><AiOutlineHome/></a>
+      <a href="#about"><AiOutlineUser/></a>
+      <a href="#experience"><BiBook/></a>
+      <a href="#portfolio"><CgWorkAlt/></a>
+      <a href="#contact"><BiMessageRoundedDetail/></a>
+    </nav>
   )
 }
 

3f68cfafc92fb61436e7ee88834dfb3d3ce38ffb
Amel Khiri <53347966+algerina@users.noreply.github.com>
2022-10-02 22:03:13 +0100

    Add portfolio section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,20 @@ import './portfolio.css'
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        <article className="portfolio__item">
+             <div className="portfolio__item-image"></div>
+             <h3>Portfolio item title</h3>
+             <a href="http://github.com" className="btn" target='_blank'>Github</a>
+             <a href="https://dribbble.com/Alien_pixels" className="btn btn-primary" target='_blank'>Live Demo</a>
+
+        </article>
+      </div>
+    </section>
   )
 }
 

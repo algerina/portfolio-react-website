de697fcec86ec05dae8da0a510c29c30e293fec3
Amel Khiri <53347966+algerina@users.noreply.github.com>
2022-10-03 11:56:11 +0100

    Add testimonials section

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import './testimonials.css'
+import AVTR1 from '../../assets/avatar1.jpg'
+import AVTR2 from '../../assets/avatar2.jpg'
+import AVTR3 from '../../assets/avatar3.jpg'
+import AVTR4 from '../../assets/avatar4.jpg'
+
 
 const Testimonials = () => {
   return (
-    <section id='testimonials'>Testimonials</section>
+    <section id='testimonials'>
+      <h5>Reviews</h5>
+      <h2>Testimonials</h2>
+      <div className="testimonials__container container">
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVTR1} alt="Avatar One" />
+            <h5>Client Name</h5>
+            <small className="client__review"></small>
+          </div>
+        </article>
+      </div>
+    </section>
   )
 }
 

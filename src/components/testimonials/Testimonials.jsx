d5cc0316d225e5dbd138fd2595e8b92d4ee1c1b9
Amel Khiri <53347966+algerina@users.noreply.github.com>
2022-10-04 11:54:46 +0100

    Install SwiperJs

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -5,69 +5,67 @@ import AVTR2 from '../../assets/avatar2.jpg'
 import AVTR3 from '../../assets/avatar3.jpg'
 import AVTR4 from '../../assets/avatar4.jpg'
 
+// import Swiper core and required modules
+import { Pagination } from 'swiper';
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+
+
+const data = [
+  {
+    avatar: AVTR1,
+    name: "name1",
+    review: "what ever what ever what ever what ever"
+  },
+  {
+    avatar: AVTR2,
+    name: "name2",
+    review: "what ever what ever what ever what ever"
+  },
+  {
+    avatar: AVTR3,
+    name: "name3",
+    review: "what ever what ever what ever what ever"
+  },
+  {
+    avatar: AVTR4,
+    name: "name4",
+    review: "what ever what ever what ever what ever"
+  }
+]
 const Testimonials = () => {
   return (
     <section id='testimonials'>
       <h5>Reviews</h5>
       <h2>Testimonials</h2>
-      <div className="testimonials__container container">
-        <article className="testimonial">
-          <div className="client__avatar">
-            <img src={AVTR1} alt="Avatar One" />
- 
-          </div>
-          <h5 className="client__name">Client Name</h5>
-            <small className="client__review">Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-             Excepturi soluta corporis eveniet eos ipsum voluptatibus numquam fuga ab voluptatem tenetur
-              deleniti asperiores sapiente optio non voluptate quaerat vero, quae aut?</small>
-        </article>
+      <Swiper className="testimonials__container container"
+      // install Swiper modules
+      modules={[Pagination]}
+      spaceBetween={40}
+      slidesPerView={1}
+      Pagination={{ clickable;true }}
 
-        <article className="testimonial">
+      >
+        { 
+       data.map(({avatar, name, review}, index) => {
+        return (
+          <SwiperSlide key={index} className="testimonial">
           <div className="client__avatar">
-            <img src={AVTR1} alt="Avatar One" />
+            <img src={avatar}  />
            
           </div>
-          <h5 className="client__name">Client Name</h5>
-            <small className="client__review">Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-             Excepturi soluta corporis eveniet eos ipsum voluptatibus numquam fuga ab voluptatem tenetur
-              deleniti asperiores sapiente optio non voluptate quaerat vero, quae aut?</small>
-        </article>
+          <h5 className="client__name">{name}</h5>
+            <small className="client__review">{review}</small>
+        </SwiperSlide>
 
-        <article className="testimonial">
-          <div className="client__avatar">
-            <img src={AVTR1} alt="Avatar One" />
-           
-          </div>
-          <h5 className="client__name">Client Name</h5>
-            <small className="client__review">Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-             Excepturi soluta corporis eveniet eos ipsum voluptatibus numquam fuga ab voluptatem tenetur
-              deleniti asperiores sapiente optio non voluptate quaerat vero, quae aut?</small>
-        </article>
-
-        <article className="testimonial">
-          <div className="client__avatar">
-            <img src={AVTR1} alt="Avatar One" />
-         
-          </div>
-          <h5 className="client__name">Client Name</h5>
-            <small className="client__review">Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-             Excepturi soluta corporis eveniet eos ipsum voluptatibus numquam fuga ab voluptatem tenetur
-              deleniti asperiores sapiente optio non voluptate quaerat vero, quae aut?</small>
-        </article>
-        
-        <article className="testimonial">
-          <div className="client__avatar">
-            <img src={AVTR1} alt="Avatar One" />
-          
-          </div>
-          <h5 className="client__name">Client Name</h5>
-            <small className="client__review">Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-             Excepturi soluta corporis eveniet eos ipsum voluptatibus numquam fuga ab voluptatem tenetur
-              deleniti asperiores sapiente optio non voluptate quaerat vero, quae aut?</small>
-        </article>
-
-      </div>
+        )
+       })
+      }
+      </Swiper>
     </section>
   )
 }

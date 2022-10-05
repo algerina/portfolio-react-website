734bf454df87293916054580409341d2ea83f52c
Amel Khiri <53347966+algerina@users.noreply.github.com>
2022-10-05 01:47:46 +0100

    Add contact section

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import './contact.css'
+import React from "react";
+import "./contact.css";
+import { MdOutlineMail } from "react-icons/md";
+import {SiWhatsapp} from 'react-icons/si'
 
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
-  )
-}
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+      <div className="contact__container container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineMail />
+            <h4>Email</h4>
+            <a href="mailto: kh.amelle@gmail.com" target="_blank">Send a message</a>
+          </article>
 
-export default Contact
+          <article className="contact__option">
+            <SiWhatsapp />
+            <h4>WhatsApp</h4>
+            <a href="https://api.whatsapp.com/send?phone=+213773538105" target="_blank">Send a message</a>
+          </article>
+
+        </div>
+        {/* End of contact options */}
+        <form action="">
+          <input type="text" name="name" placeholder="Your Full Name" required />
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;

188b8dff5fa8ad93a426742039320d8d20a605f9
Amel Khiri <53347966+algerina@users.noreply.github.com>
2022-10-29 00:13:31 +0100

    Style footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,29 +1,56 @@
-import React from 'react'
-import './footer.css'
+import React from "react";
+import "./footer.css";
+import { FaFacebookF } from "react-icons/fa";
+import { FiInstagram } from "react-icons/fi";
+import { FiTwitter } from "react-icons/fi";
 
 const Footer = () => {
   return (
     <footer>
-      <a href="#" className='footer__logo'>Algerina</a>
-      <ul className='permalinks'>
-        <li><a href="#">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#experience">Experience</a></li>
-        <li><a href="#services">Services</a></li>
-        <li><a href="#portfolio">Portfolio</a></li>
-        <li><a href="#testimonials">Testimonials</a></li>
-        <li><a href="#contact">Contact</a></li>
-
+      <a href="#" className="footer__logo">
+        Algerina
+      </a>
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
       </ul>
 
-<div className='footer__socials'>
-  <a href="http://facebook.com"></a>
-  <a href="http://instagram.com"></a>
-  <a href="http://twitter.com"></a>
-</div>
+      <div className="footer__socials">
+        <a href="http://facebook.com">
+          <FaFacebookF />
+        </a>
+        <a href="http://instagram.com">
+          <FiInstagram />
+        </a>
+        <a href="http://twitter.com">
+          <FiTwitter />
+        </a>
+      </div>
 
+      <div className="footer__copyright">
+        <small>&copy; Algerina. All rights reserved.</small>
+      </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;

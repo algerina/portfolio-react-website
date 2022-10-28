cb0fe2428d93ccdda570d35ccdb7a3f884658846
Amel Khiri <53347966+algerina@users.noreply.github.com>
2022-10-28 23:14:56 +0100

    Add footer section

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,26 @@ import './footer.css'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>Algerina</a>
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contact">Contact</a></li>
+
+      </ul>
+
+<div className='footer__socials'>
+  <a href="http://facebook.com"></a>
+  <a href="http://instagram.com"></a>
+  <a href="http://twitter.com"></a>
+</div>
+
+    </footer>
   )
 }
 

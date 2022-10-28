efa4cc04798f325914abfaed009e5917f28807d7
Amel Khiri <53347966+algerina@users.noreply.github.com>
2022-10-28 22:08:58 +0100

    Install and link emailjs

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,8 +2,18 @@ import React from "react";
 import "./contact.css";
 import { MdOutlineMail } from "react-icons/md";
 import {SiWhatsapp} from 'react-icons/si'
+import { useRef } from 'react'
+import emailjs from 'emailjs-com'
 
 const Contact = () => {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_gy5w5ns', 'template_nv09ggr', form.current, 'QcWrvegg-9A41UAdg')
+    e.target.reset()
+     
+    };
   return (
     <section id="contact">
       <h5>Get In Touch</h5>
@@ -24,7 +34,7 @@ const Contact = () => {
 
         </div>
         {/* End of contact options */}
-        <form action="">
+        <form ref={form} onSubmit={sendEmail}>
           <input type="text" name="name" placeholder="Your Full Name" required />
           <input type="email" name="email" placeholder="Your Email" required />
           <textarea name="message" rows="7" placeholder="Your Message" required ></textarea>
